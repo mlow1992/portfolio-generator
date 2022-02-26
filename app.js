6649430e75f28d38f706c0a05ccd465a7a70ab72
General Zelgius <mattlow123@gmail.com>
2022-02-26 12:54:33 -0500

    added more HTML and dynamic user input

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-// const fs = require('fs');
+const fs = require('fs');
 const inquirer = require('inquirer');
-// const generatePage = require('./src/page-template.js');
+const generatePage = require('./src/page-template.js');
 
 const promptUser = () => {
     return inquirer.prompt ([
@@ -57,7 +57,7 @@ const promptProject = portfolioData => {
     Add a New Project
     =================
     `);
-    // IF there's no 'projects' array property, create one
+    // If there's no 'projects' array property, create one
     if (!portfolioData.projects) {
         portfolioData.projects = [];
     }
@@ -133,12 +133,11 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+        
+        fs.writeFile('./index.html', pageHTML, err => {
+            if (err) throw err;
+
+            console.log('Portfolio complete! Check out index.html to see the output!');
     });
-// const pageHTML = generatePage(nName, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-    //     if (err) throw err;
-
-//     console.log('Portfolio complete! Check out index.html to see the output!');
-// });
+});
