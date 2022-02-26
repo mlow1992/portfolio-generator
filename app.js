5ca9b321a954e5c1ced584ccba59c5867cba22f7
General Zelgius <mattlow123@gmail.com>
2022-02-26 13:46:57 -0500

    added promises and refactored code

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const fs = require('fs');
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template.js');
+const { writeFile, copyFile } = require('./utils/generate-site');
 
 const promptUser = () => {
     return inquirer.prompt ([
@@ -133,11 +133,18 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        const pageHTML = generatePage(portfolioData);
-        
-        fs.writeFile('./index.html', pageHTML, err => {
-            if (err) throw err;
-
-            console.log('Portfolio complete! Check out index.html to see the output!');
+        return generatePage(portfolioData);
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML);
+    })
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+    })
+    .then(copyFileResponse => {
+        console.log(copyFileResponse);
+    })
+    .catch(err => {
+        console.log(err);
     });
-});
